9a7762608d5f3c48fd4a4c18b8afca2c02efc80d
Mike Shahindoust <gobias101@gmail.com>
2020-08-25 23:30:43 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,8 +3,9 @@ class Cat
   attr_reader :name
   attr_accessor :owner
   
-  def initialize(name, owner)
+  def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
+    @mood = mood
   end
 end
